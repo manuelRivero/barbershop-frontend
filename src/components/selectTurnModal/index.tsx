d8771bcb0c93ad47a6005f458e9b1de6d7c73876
manuel rivero <manuel.enrique.r.v@gmail.com>
2023-10-03 22:06:31 -0300

    [add] add turn confirmation modal

--- src/components/selectTurnModal/index.tsx
+++ src/components/selectTurnModal/index.tsx
@@ -20,17 +20,63 @@ import {
 import {TurnSelectItem} from '../../types/turns';
 import {ListRenderItemInfo} from 'react-native';
 import moment from 'moment';
+import {useAppDispatch} from '../../store';
+import {showInfoModal} from '../../store/features/layoutSlice';
 
 interface Props {
   show: boolean;
   onClose: () => void;
   turns: TurnSelectItem[];
-  onSelect : (e:TurnSelectItem) => void
+  onSelect: (e: TurnSelectItem) => void;
 }
 
-export default function SelectTurnModal({show, onClose, turns, onSelect}: Props) {
+export default function SelectTurnModal({
+  show,
+  onClose,
+  turns,
+  onSelect,
+}: Props) {
+  const dispatch = useAppDispatch();
   const ref = useRef();
 
+  const handleSelect = (item: TurnSelectItem): void => {
+    dispatch(
+      showInfoModal({
+        title: `¡Deseas agendar este turno ${moment(item.startDate).format(
+          'hh:mm',
+        )}?`,
+        type: 'info',
+        hasCancel: true,
+        cancelCb: () => onClose(),
+        hasSubmit: true,
+        submitCb: () => {
+          onSelect(item);
+          dispatch(
+            showInfoModal({
+              title: '¡Turno agendado!',
+              type: 'success',
+              hasCancel: false,
+              cancelCb: null,
+              hasSubmit: false,
+              submitCb: null,
+              hideOnAnimationEnd: true,
+              submitData: null,
+              cancelData: null,
+            }),
+          );
+        },
+        hideOnAnimationEnd: false,
+        submitData: {
+          text: 'Agendar',
+          background: '$green500',
+        },
+        cancelData: {
+          text: 'Cancelar',
+          background: '$blueGray200',
+        },
+      }),
+    );
+  };
   return (
     <Modal isOpen={show} onClose={onClose} finalFocusRef={ref}>
       <ModalBackdrop />
@@ -57,11 +103,10 @@ export default function SelectTurnModal({show, onClose, turns, onSelect}: Props)
             renderItem={(props: ListRenderItemInfo<any>) => {
               const {item} = props;
               return (
-                <Pressable onPress={()=> onSelect(item)}>
-                <Box softShadow={'1'} p="$4" borderRadius="$lg" bg="$white">
-                  <Text>{moment(item.startDate).format('hh:mm')}</Text>
-                </Box>
-
+                <Pressable onPress={() => handleSelect(item)}>
+                  <Box softShadow={'1'} p="$4" borderRadius="$lg" bg="$white">
+                    <Text>{moment(item.startDate).format('hh:mm')}</Text>
+                  </Box>
                 </Pressable>
               );
             }}
